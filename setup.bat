d95fb0e94825e735ebcded4bde0331e4cd6bb3e6
OASIS0110 <oasis.sub0110@gmail.com>
2025-09-21 02:10:17 +0900

    👷 環境構築にテコ入れをした。
    - セットアップファイルにシバンを追加。
    - cmdでもセットアップできるように。
    - 環境変数をdockerにコピーできていなかった問題を修正
    - dev環境時、hot reloadするように。
    - ts-nodeからtsxに変更。

---
+++ setup.bat
@@ -0,0 +1,5 @@
+call docker compose down --rmi all
+mkdir decrypted_env
+call dotenvx decrypt -f .env.local --stdout > ./decrypted_env/.env.local
+call docker compose build
+call docker compose up -d
--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+
+set -eu
 docker compose down --rmi all
 dotenvx decrypt -f .env.local --stdout > ./decrypted_env/.env.local
 docker compose build
